71f3d65770bf64123b502e39b981cfe3cb7e4cc1
JUMATION2 <jumatovu9@gmail.com>
2025-09-02 22:40:57 +0300

    Am studying Git

---
+++ whileloop.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+
+int main() {
+/*
+int num1;
+int count = 10;
+printf("Insert a number:");
+scanf("%d", &num1);
+
+ while (count >0) {
+
+    int result = num1*count;
+
+    printf("%d*%d = %d\n",num1,count,result);
+
+count = count -1;
+}; */
+
+int num2;
+
+printf("Insert an Figure:");
+scanf("%d",&num2);
+
+
+ while() {
+if ( num2 %2!=0 && num2<0 ) {
+   printf("Here is %d...............It qualifies",num2);
+
+} else if(num2<0) {
+
+   printf("This is a negative value but doesn't qualify ");
+
+} else {
+
+   printf("This is a positive value and doesn't qualify ");
+
+}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+return 0;
+}
